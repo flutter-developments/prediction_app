0013525ad1333d7bcd25518e5f8e8ccc04a2b848
farhanali-98 <al_adl7668@outlook.com>
2021-09-09 00:01:00 +0500

    google play store payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
-
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:provider/provider.dart';
+ 
+import 'provider/providermodel.dart';
+import 'ui/home_screen.dart';
+ 
 void main() {
-  runApp(MyApp());
+  InAppPurchaseConnection.enablePendingPurchases();
+ 
+  runApp(ChangeNotifierProvider(
+      create: (context) => ProviderModel(),
+      child: MaterialApp(
+        home: MyApp(),
+      )));
 }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+ 
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    var provider = Provider.of<ProviderModel>(context, listen: false);
+    provider.initialize();
+    super.initState();
+  }
+ 
+  @override
+  void dispose() {
+    var provider = Provider.of<ProviderModel>(context, listen: false);
+    provider.subscription.cancel();
+    super.dispose();
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return HomeScreen();
   }
 }
