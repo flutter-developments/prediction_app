a21be7a528445529c843ccb07f100f68d33d4d8f
Moeez1999 <moeez.suleman460@gmail.com>
2021-09-13 20:25:22 +0500

    esfsdf

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prediction_app/ui/home/exchange_history.dart';
-
+import 'package:prediction_app/ui/user_guide/user_guide3.dart';
 
 void main() => runApp(MyApp());
 void configLoading() {}
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      builder: () => MaterialApp(
-          debugShowCheckedModeBanner: false, home: ExchangeHistory()),
+      builder: () =>
+          MaterialApp(debugShowCheckedModeBanner: false, home: UserGuide3()),
       designSize: Size(414, 896),
     );
   }
